e92235a7d47abb51c502e379825c3186d7aa9089
Rafael <rafaelmaxwell.silva@gmail.com>
2025-10-16 07:18:33 -0300

    Created the Cart Class

---
+++ javascript/data/cart-class.js
@@ -0,0 +1,47 @@
+class Cart{
+
+  localStorageKey;
+  cartItems = JSON.parse(localStorage.getItem(this.localStorageKey)) || [];
+
+  constructor(localStorageKey){
+    this.localStorageKey = localStorageKey;
+  }
+
+  addToCart(id, quantity) {
+
+    const sameItem = this.cartItems.find(cartItem => cartItem.id === id);
+
+    if(sameItem) {
+      sameItem.quantity += quantity;
+    } else {
+      this.cartItems.push({
+        id,
+        quantity,
+        deliveryOptionId: '1'
+      })
+    }
+    this.saveCartLocal();
+  };
+
+  saveCartLocal() {
+    localStorage.setItem(this.localStorageKey, JSON.stringify(this.cartItems));
+  };
+
+  updateCartQuantity() {
+
+    let cartQuantity = 0;
+
+    this.cartItems.forEach(cartItem => cartQuantity += cartItem.quantity);
+
+    return cartQuantity;
+  };
+
+}
+
+const cart = new Cart('cart-oop');
+const businessCart = new Cart('cart-business');
+
+
+console.log(cart);
+console.log(businessCart);
+console.log(businessCart instanceof Cart);
